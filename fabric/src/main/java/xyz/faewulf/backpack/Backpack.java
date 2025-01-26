96bce81ea4b3a05cbf506977b2a865aa90d88b68
FaeWulfs <ngolamaz3@gmail.com>
2025-01-26 08:58:31 +0700

    + Add: 5s interval to fetch backpack data for outsider player.

--- fabric/src/main/java/xyz/faewulf/backpack/Backpack.java
+++ fabric/src/main/java/xyz/faewulf/backpack/Backpack.java
@@ -1,7 +1,9 @@
 package xyz.faewulf.backpack;
 
+import com.mojang.datafixers.kinds.Const;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -12,9 +14,18 @@ import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.ResourceManager;
 import xyz.faewulf.backpack.feature.backpacks.basketBackpack.BasketBackpackModel;
 import xyz.faewulf.backpack.feature.backpacks.defaultBackPack.DefaultBackpackModel;
+import xyz.faewulf.backpack.platform.Services;
 import xyz.faewulf.backpack.registry.ItemTagRegistry;
+import xyz.faewulf.backpack.util.DataSync;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.WeakHashMap;
 
 public class Backpack implements ModInitializer {
+
+    private int timer = 0;
 
     @Override
     public void onInitialize() {
@@ -42,21 +53,55 @@ public class Backpack implements ModInitializer {
 
     private void loadEvent() {
         Constants.LOG.info("Register events...");
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-        });
 
-        // resource reload event
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-            @Override
-            public ResourceLocation getFabricId() {
-                return null;
-            }
+        // Client only event
+        if (Services.PLATFORM.isClientSide()) {
 
-            @Override
-            public void onResourceManagerReload(ResourceManager resourceManager) {
-                ItemTagRegistry.loadAllBackpackItems();
-            }
-        });
+            // Todo: port for neoforge
+            // Client tick event
+            // For fetching not yet update player's backpack data
+            ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
+                if (minecraft.level == null || minecraft.player == null)
+                    return;
+
+                timer++;
+
+                if (timer >= 100) {
+                    timer = 0;
+
+                    Constants.PLAYER_INV_STATUS.forEach((s, backpackStatus) -> {
+                        // Skip LocalPlayer
+                        if (s.equals(Constants.DUMMY_PLAYER_NAME) || s.equals(minecraft.player.getName().getString()))
+                            return;
+
+                        // Max 20 to avoid api rate limit
+                        if (DataSync.UPDATE_QUEUE.size() > 20)
+                            return;
+
+                        if (!backpackStatus.hasUpdateBackpackType && backpackStatus.uuid != null) {
+                            DataSync.UPDATE_QUEUE.put(s, backpackStatus.uuid);
+                        }
+                    });
+
+                    DataSync.requestUpdateData();
+                }
+            });
+
+            // resource reload event
+            ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+                @Override
+                public ResourceLocation getFabricId() {
+                    return null;
+                }
+
+                @Override
+                public void onResourceManagerReload(ResourceManager resourceManager) {
+                    ItemTagRegistry.loadAllBackpackItems();
+                }
+            });
+
+
+        }
 
 
     }
