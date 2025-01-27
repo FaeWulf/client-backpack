fdfd79f5ed1f59049e9d83cb3028cabf82c8a806
FaeWulfs <ngolamaz3@gmail.com>
2025-01-27 17:02:41 +0700

    # Fix: Corrected the config name.

--- common/src/main/java/xyz/faewulf/backpack/util/config/ModConfigs.java
+++ common/src/main/java/xyz/faewulf/backpack/util/config/ModConfigs.java
@@ -2,19 +2,19 @@ package xyz.faewulf.backpack.util.config;
 
 public class ModConfigs {
 
-    @Entry(category = "Client Only", name = "Enable mod", require_restart = false)
+    @Entry(category = "Client", name = "Enable mod", require_restart = false)
     public static boolean __enable_mod = true;
 
-    @Entry(category = "Client Only", name = "Enable Cloud Sync", require_restart = false)
+    @Entry(category = "Client", name = "Enable Cloud Sync", require_restart = false)
     public static boolean _enable_cloud_sync = true;
 
-    @Entry(category = "Client Only", name = "Enable dynamic light", require_restart = false)
+    @Entry(category = "Client", name = "Enable dynamic light", require_restart = false)
     public static boolean enable_dynamiclight_compat = true;
 
-    @Entry(category = "Client Only", name = "Hide backpack if invisible", require_restart = false)
+    @Entry(category = "Client", name = "Hide backpack if invisible", require_restart = false)
     public static boolean hide_if_invisible = true;
 
-    @Entry(category = "Server Only", name = "Hide items component data", require_restart = false)
+    @Entry(category = "Server", name = "Hide items component data", require_restart = false)
     public static boolean hide_items_component_data = false;
 
     @Entry(category = "style", name = "Backpack Model Type", require_restart = false, hidden = true)
